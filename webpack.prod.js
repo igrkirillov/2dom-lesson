fa3b8e4b3ece1ab73cdcb1d606087eacc49030a9
Igor.Kirillov <Igor.Kirillov@x5.ru>
2024-03-11 23:39:56 +0300

    4 commit

--- webpack.prod.js
+++ webpack.prod.js
@@ -5,6 +5,7 @@ const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 module.exports = merge(common, {
   mode: "production",
   optimization: {
+    minimize: false, // не получается настроить extract css
     minimizer: [new CssMinimizerPlugin()],
   },
 });
